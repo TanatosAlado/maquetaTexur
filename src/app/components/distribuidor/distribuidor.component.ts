d93ce52b4964fd0e797a9ad6de5613cd61710707
Andres Hosch <andreshosch.com>
2024-05-27 23:46:39 -0300

    agrgando grafico

--- src/app/components/distribuidor/distribuidor.component.ts
+++ src/app/components/distribuidor/distribuidor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-distribuidor',
@@ -15,10 +16,13 @@ export class DistribuidorComponent {
   ventas: boolean = false
   analisisDatos: boolean = false
   suscripciones: boolean = false
+  detallesDistribuidor:Boolean=false
   selected: string = 'perfilDistribuidor'
   arrayClientes: any[] = [{nombre: 'Juan', apellido:'Perez', cuit: '11-111111111-1',telefono:123456789},{nombre: 'Manuel', apellido:'Gomez', cuit: '22-111111111-2',telefono:987654321},{nombre: 'Mauro', apellido:'Garcia', cuit: '33-111111111-3',telefono:789789788},{nombre: 'Alberto', apellido:'Sanchez', cuit: '44-111111111-4',telefono:478885547},]
 
+  constructor(private router: Router){
 
+  }
 
   cambio(parametro: string){
     this.perfilDistribuidor=false
@@ -31,12 +35,15 @@ export class DistribuidorComponent {
     this.analisisDatos= false
     this.suscripciones= false
 
+
     switch (parametro) {
       case 'perfilDistribuidor': 
       this.perfilDistribuidor = true
+      this.detallesDistribuidor=false
       break;
       case 'gestionCliente': 
         this.gestioncliente = true
+        this.detallesDistribuidor=false
         break;
       case 'postVenta': 
         this.postVenta = true
@@ -69,6 +76,13 @@ export class DistribuidorComponent {
 
   }
   mostrarDetalle(){
-console.log("hola")
+this.gestioncliente=false
+this.detallesDistribuidor=true
+  }
+
+  borrarCliente(){
+   alert("Cliente borrado con éxito")
+      this.router.navigate(['distribuidor'])
+
   }
 }
